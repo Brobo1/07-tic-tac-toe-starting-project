40f54806f2b14b356297648a396ffdfbdb72a454
brobo1 <armanahz95@gmail.com>
2024-02-16 17:48:56 +0100

    Add gameplay functionality and other enhancements to Tic-Tac-Toe app
    Added several React components to implement the main functions of the game like managing player activities, showing game over state, and managing game board. Implemented functionality to derive active player and winner. Also added the game's winning combinations and updated the UI with headers and logos. React and react-dom dependencies got an update and a new dependency was added.

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,37 @@
+
+
+export function GameBoard({onSelectSquare, board}) {
+
+
+	//const [gameBoard, setGameBoard] = useState(initialBoard);
+
+	//function handleSelectedSquare(rowIndex, colIndex) {
+	//	setGameBoard((prevGameBoard) => {
+	//		const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+	//		updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+	//		return updatedBoard;
+	//	});
+	//	onSelectSquare();
+	//}
+
+	return (
+		<ol id={"game-board"}>
+			{board.map((row, rowIndex) => (
+				<li key={rowIndex}>
+					<ol>
+						{row.map((playerSymbol, colIndex) => (
+							<li key={colIndex}>
+								<button
+									onClick={() => onSelectSquare(rowIndex, colIndex)}
+									disabled={playerSymbol !== null}
+								>
+									{playerSymbol}
+								</button>
+							</li>
+						))}
+					</ol>
+				</li>
+			))}
+		</ol>
+	)
+}
